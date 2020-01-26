19d73a7ba25cf0b181a3972fdc8470e486382e6b
mckriskr <mckriskr@gmail.com>
2020-01-27 00:51:11 +0900

    create management system app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': 'hong kil dong',
+    'birthday': '961222',
+    'gender': 'man',
+    'job': 'college student'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': 'na dong bin',
+    'birthday': '960508',
+    'gender': 'man',
+    'job': 'programmer'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': 'lee sun sin',
+    'birthday': '961127',
+    'gender': 'man',
+    'job': 'designer'
+  }
+]
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c=>{
+          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
